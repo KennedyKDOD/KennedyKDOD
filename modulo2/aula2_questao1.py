65bcf7df4edbbd3c473e6d5dd9c5667caba93ec6
KennedyKDOD <164103462+KennedyKDOD@users.noreply.github.com>
2024-05-03 13:57:59 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,11 @@
+a= 5
+b= 2
+
+print("O tipo de dado da variável a é")
+print(type(a))
+print("E o tipo de dado da variável b é")
+print(type(b))
+
+divisao= a/b
+print(divisao)
+print(type(divisao))
